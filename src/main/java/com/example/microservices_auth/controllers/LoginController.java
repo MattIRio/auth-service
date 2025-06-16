e38df10183a98d1cb783e5a3fda4da803459c724
MattIRio <matlirioiii@gmail.com>
2025-06-16 13:24:00 +0300

    w

--- src/main/java/com/example/microservices_auth/controllers/LoginController.java
+++ src/main/java/com/example/microservices_auth/controllers/LoginController.java
@@ -3,12 +3,14 @@ package com.example.microservices_auth.controllers;
 import com.common.lib.authModule.authDto.LoginCredentialsDto;
 import com.example.microservices_auth.service.MyUserDetailService;
 import com.example.microservices_auth.webtoken.JwtService;
+import jakarta.ws.rs.GET;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +34,9 @@ public class LoginController {
         } else {
             throw new UsernameNotFoundException("Invalid credentials");
         }
+    }
+    @GetMapping("/test")
+    public String test() {
+       return "test passed";
     }
 }
