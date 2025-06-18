b291b6216b3293a1f4358de8e91c952adf03ddb0
MattIRio <matlirioiii@gmail.com>
2025-06-18 16:10:23 +0300

    ц

--- src/main/java/com/example/microservices_auth/AuthApplication.java
+++ src/main/java/com/example/microservices_auth/AuthApplication.java
@@ -2,7 +2,6 @@ package com.example.microservices_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
