ebbf6e480f7ca277070686314f62e34bfa6ce488
MattIRio <matlirioiii@gmail.com>
2025-06-18 16:09:03 +0300

    ц

--- src/main/java/com/example/microservices_auth/AuthApplication.java
+++ src/main/java/com/example/microservices_auth/AuthApplication.java
@@ -2,12 +2,14 @@ package com.example.microservices_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 @EnableWebSecurity
 public class AuthApplication {
